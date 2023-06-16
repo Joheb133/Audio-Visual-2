6212fdacbd618f7cb88988fb2a52e405731335f9
zoheb598 <joheb133@gmail.com>
2023-06-16 17:46:34 +0100

    add visualiser selector img & gif

--- src/components/Visualiser/Visualiser.tsx
+++ src/components/Visualiser/Visualiser.tsx
@@ -5,13 +5,44 @@ interface VisualiserProp {
   analyser: AnalyserNode | undefined;
 }
 
+type VisualisationObject = {
+  [key: string]: {
+    code: new (
+      ctx: CanvasRenderingContext2D,
+      analyser: AnalyserNode,
+      width?: number,
+      height?: number
+    ) => any;
+    img: {
+      default: string;
+      large: string;
+    };
+    gif: string;
+  };
+};
+
 export default function Visualiser({ analyser }: VisualiserProp) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [visual, setVisual] = useState(1);
-  const visualDictionary: { [key: number]: typeof Bar | typeof Circle } = {
-    1: Bar,
-    2: Circle,
+  const [visual, setVisual] = useState("Bar");
+  const visualData: VisualisationObject = {
+    Bar: {
+      code: Bar,
+      img: {
+        default: "img/bar_default.png",
+        large: "img/bar_large.png",
+      },
+      gif: "gif/bar.gif",
+    },
+    Circle: {
+      code: Circle,
+      img: {
+        default: "img/bar_default.png",
+        large: "img/bar_large.png",
+      },
+      gif: "gif/bar.gif",
+    },
   };
+  const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
     if (!analyser || !canvasRef.current) return;
@@ -23,7 +54,7 @@ export default function Visualiser({ analyser }: VisualiserProp) {
     const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
     if (!ctx || !vContainer) return;
 
-    const visualisation = new visualDictionary[visual](ctx, analyser);
+    const visualisation = new visualData[visual].code(ctx, analyser);
 
     //resize canvas
     const canvasResize = () => {
@@ -55,20 +86,18 @@ export default function Visualiser({ analyser }: VisualiserProp) {
       <canvas ref={canvasRef} className="w-full h-full absolute"></canvas>
       <div className="visual-selector-wrap w-full h-28 box-border flex absolute bottom-0 gap-4 items-center justify-center">
         <div
-          className="w-32 h-16 bg-white rounded-lg left-1/2 hover:cursor-pointer"
+          className="w-32 h-16 left-1/2 hover:cursor-pointer border rounded-lg"
           onClick={() => {
-            setVisual(1);
+            setVisual("Bar");
           }}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
         >
-          Bar
-        </div>
-        <div
-          className="w-32 h-16 bg-white rounded-lg left-1/2 hover:cursor-pointer"
-          onClick={() => {
-            setVisual(2);
-          }}
-        >
-          Circle
+          <img
+            src={isHovered ? visualData.Bar.gif : visualData.Bar.img.default}
+            alt=""
+            className="w-full h-full rounded-lg"
+          />
         </div>
       </div>
     </div>
